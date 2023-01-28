fb36b41e600319784016bd39fd9ead6bd6c41da1
Mônica Marçal <monicagmarcal@outlook.com>
2023-01-28 17:10:08 -0300

    notificação push mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,7 @@
 import './src/lib/dayjs';
 import { StatusBar } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
 import { 
   useFonts,
   Inter_400Regular,
@@ -10,6 +12,7 @@ import {
 
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
+import { useEffect } from 'react';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,6 +21,39 @@ export default function App() {
     Inter_700Bold,
     Inter_800ExtraBold
   });
+
+  
+
+  //pegando notificações agendadas
+  async function schedulePushNotification() {
+   const schedule = await Notifications.getAllScheduledNotificationsAsync();
+   console.log("Agendadas: ", schedule);
+  
+
+   if (schedule.length > 0) {
+     await Notifications.cancelAllScheduledNotificationsAsync();
+    }
+
+    //adicionando tempo para enviar a notificação
+    const trigger = new Date(Date.now());
+    trigger.setHours(trigger.getHours() + 1);
+    trigger.setSeconds(0);
+    //trigger.setMinutes(trigger.getMinutes() + 1)
+
+    //apresentação da notificação 
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Olá, Mônica! 😀",
+        body: "Você praticou seus hábitos hoje?"
+      },
+      trigger
+    });
+  }
+
+  useEffect(() => {
+    schedulePushNotification();
+  }, []);
+
   if (!fontsLoaded) {
     return (
       <Loading />
